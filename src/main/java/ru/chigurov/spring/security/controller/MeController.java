16eedcd390e22a5d28a1bc47bae807c333e69742
MikHail854 <vip.moneta95@mail.ru>
2021-09-29 12:51:26 +0700

    authentication procedure

---
+++ src/main/java/ru/chigurov/spring/security/controller/MeController.java
@@ -0,0 +1,25 @@
+package ru.chigurov.spring.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MeController {
+
+    @GetMapping("/")
+    public String getInfoForAllEmps(){
+        return "view_for_all_employees";
+    }
+
+
+    @GetMapping("hr_info")
+    public String getInfoOnlyForHR(){
+        return "view_for_hr";
+    }
+
+    @GetMapping("/manager_info")
+    public String getInfoOnlyForManagers(){
+        return "view_for_managers";
+    }
+
+}
